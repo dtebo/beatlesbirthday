942784c5aa440efc1e0293eb2f1be0171470b8a7
Darren Tebo <darrentebo83@outlook.com>
2020-09-12 10:54:07 -0400

    updated background

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import beatles from '../../../assets/beatles.gif';
+import beatles from '../../assets/beatles.gif';
 
 const Header = props => {
     return (
         <div className='App-header'>
             <section className='title-container'>
                 <h1>Happy Birthday Dad!</h1>
+                <img src={beatles} alt='beatles birthday wishes' />
             </section>
         </div>
     );
