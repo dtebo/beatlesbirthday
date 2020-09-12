e85c94d7f316ed5b15a78423711ddc43a5960b0f
Darren Tebo <darrentebo83@outlook.com>
2020-09-12 10:40:09 -0400

    moved assets directory

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import beatles from '../../../assets/beatles.gif';
+
+const Header = props => {
+    return (
+        <div className='App-header'>
+            <section className='title-container'>
+                <h1>Happy Birthday Dad!</h1>
+            </section>
+        </div>
+    );
+};
+
+export default Header;
